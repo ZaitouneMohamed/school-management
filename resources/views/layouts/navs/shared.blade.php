7bf750a6573ba8c630b31c60c65fadaf74bbf6e7
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-01-16 16:54:14 +0100

    add layouts

---
+++ resources/views/layouts/navs/shared.blade.php
@@ -0,0 +1,8 @@
+<li class="nav-item">
+    <a href="{{ url('/logout') }}" class="nav-link">
+        <i class="nav-icon fas fa-th"></i>
+        <p>
+            Log Out
+        </p>
+    </a>
+</li>
