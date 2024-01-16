15a5153e1fba54853f38a88793d14ba618385686
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-01-16 23:37:52 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Subject::factory(20)->create();
+        \App\Models\Classe::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
