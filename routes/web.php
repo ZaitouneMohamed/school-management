24ec9bee88f273c2db4375b70822b87ff4731d83
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-01-16 18:54:52 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(AuthController::class)->name("auth.")->group(function () {
+    Route::get('/', 'login')->name("login.view")->middleware("guest");
+    Route::post('/login', 'AuthController')->name("login.function");
+    Route::get('/logout', 'logout')->name("logout");
+
+    Route::get('/forget-password', 'ForgetPasswordView')->name("forget-password.view");
+    Route::post('/forget-password', 'ForgetPassword')->name("forget-password.function");
+
+    Route::get('/reset-password/{token}', 'showResetPasswordForm')->name("reset-password.view");
+    Route::post('/reset-password', 'submitResetPasswordForm')->name("reset-password.function");
+});
+
+Route::middleware("admin")->name("admin.")->group(function () {
+    //Admin
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name("home");
+});
+
+Route::middleware("student")->name("student.")->group(function () {
+    //Student
+    Route::get('/student', function () {
+        return view('student.dashboard');
+    })->name("home");
+});
+
+Route::middleware("teacher")->name("teacher.")->group(function () {
+    //Teacher
+    Route::get('/teacher', function () {
+        return view('teacher.dashboard');
+    })->name("home");
+});
+
+Route::middleware("parent")->name("parent.")->group(function () {
+    //Parent
+    Route::get('/parent', function () {
+        return view('parent.dashboard');
+    })->name("home");
 });
